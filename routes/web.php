02285ed225332e4ae569d91b1964133fd310fbce
brend4ds <104913796+brend4ds@users.noreply.github.com>
2024-12-13 10:48:21 -0300

    aula 13/12

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,14 @@ Route::post('login', [UsuariosController::class,'login']);
 
 //Logout
 Route::get('logout', [UsuariosController::class, 'logout'])->name('logout');
+
+//User
+Route::prefix('usuarios')->middleware('auth')->group(function(){
+    Route::get('/', [UsuariosController::class, 'index'])->name('usuarios');
+    Route::get('/cadastrar', [UsuariosController::class, 'cadastrar'])->name('usuarios.cadastrar');
+    Route::post('/cadastrar', [UsuariosController::class, 'gravar'])->name('usuarios.gravar');
+    //Route::get('/apagar/{usuario}', [UsuariosController::class, 'apagar'])->name('usuarios.apagar');
+    //Route::delete('/apagar/{usuario}', [UsuariosController::class,'deletar']);
+    //Route::get('/editar/{usuario}', [UsuariosController::class, 'editar'])->name('usuarios.editar');
+    //Route::put('/editar/{usuario}', [UsuariosController::class, 'editarGravar']);
+});
